ae9ffb8ee4318f03665c5f2adbbce2e48edd0c60
TheQolitum <qolitum@gmail.com>
2022-09-08 17:37:47 +0700

    Main

--- test.js
+++ test.js
@@ -12,19 +12,21 @@ var controller = {
             $('.in').val('');
         });
         $('.word').click(function () {
-            let index;
-            for (index = 0; index < arr.length; ++index) {
-                if (arr[index] == 0) {
-                    arr[index] = arr[index];
-                } else if (arr[index] % 3 == 0 && arr[index] % 5 == 0) {
-                    arr[index] = 'apple and orange';
-                } else if (arr[index] % 3 == 0 && arr[index] != 0) {
-                    arr[index] = 'apple';
-                } else if (arr[index] % 5 == 0 && arr[index] != 0) {
-                    arr[index] = 'orange';
+            arr.forEach(function (value, i) {
+                if (value != 0) {
+                    let str = [];
+                    if (value % 3 == 0) {
+                        str.push('apple');
+                    }
+                    if (value % 5 == 0) {
+                        str.push('orange');
+                    }
+                    if (str.length) {
+                        arr[i] = str.join(' and ');
+                    }
                 }
-                $('.out').val(arr);
-            }
+            })
+            $('.out').val(arr);
         });
     }
 }
